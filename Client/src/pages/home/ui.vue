7cf2998af24c380b7f8d526c4b8c82bc92d5b10c
Edanriell <Edanriell@gmail.com>
2024-07-18 13:49:52 +0300

    finished mobile ui

--- Client/src/pages/home/ui.vue
+++ Client/src/pages/home/ui.vue
@@ -1,9 +1,13 @@
 <script lang="ts" setup>
 	import { MainLayout } from "@widgets/layouts/main";
+
+	import { SocialProofSection } from "./sections/social-proof";
 </script>
 
 <template>
-	<MainLayout></MainLayout>
+	<MainLayout>
+		<SocialProofSection />
+	</MainLayout>
 </template>
 
 <style scoped></style>
